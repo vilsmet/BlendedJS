5d9b903a676f16cfd3b8334bde469be500c381da
Volodymyr Smetana <vilsmet@gmail.com>
2023-03-17 21:47:28 +0100

    lesson3-0

--- js/lesson2.js
+++ js/lesson2.js
@@ -105,93 +105,93 @@ makeProduct("Холодильник", 10000, logger); */
 
 // makeProduct("Холодильник", 10000, showProduct);
 
-const scientists = [
-  {
-    name: "Albert",
-    surname: "Einstein",
-    born: 1879,
-    dead: 1955,
-    id: 1,
-  },
-  {
-    name: "Isaac",
-    surname: "Newton",
-    born: 1643,
-    dead: 1727,
-    id: 2,
-  },
-  {
-    name: "Galileo",
-    surname: "Galilei",
-    born: 1564,
-    dead: 1642,
-    id: 3,
-  },
-  {
-    name: "Marie",
-    surname: "Curie",
-    born: 1867,
-    dead: 1934,
-    id: 4,
-  },
-  {
-    name: "Johannes",
-    surname: "Kepler",
-    born: 1571,
-    dead: 1630,
-    id: 5,
-  },
-  {
-    name: "Nicolaus",
-    surname: "Copernicus",
-    born: 1473,
-    dead: 1543,
-    id: 6,
-  },
-  {
-    name: "Max",
-    surname: "Planck",
-    born: 1858,
-    dead: 1947,
-    id: 7,
-  },
-  {
-    name: "Katherine",
-    surname: "Blodgett",
-    born: 1898,
-    dead: 1979,
-    id: 8,
-  },
-  {
-    name: "Ada",
-    surname: "Lovelace",
-    born: 1815,
-    dead: 1852,
-    id: 9,
-  },
+// const scientists = [
+//   {
+//     name: "Albert",
+//     surname: "Einstein",
+//     born: 1879,
+//     dead: 1955,
+//     id: 1,
+//   },
+//   {
+//     name: "Isaac",
+//     surname: "Newton",
+//     born: 1643,
+//     dead: 1727,
+//     id: 2,
+//   },
+//   {
+//     name: "Galileo",
+//     surname: "Galilei",
+//     born: 1564,
+//     dead: 1642,
+//     id: 3,
+//   },
+//   {
+//     name: "Marie",
+//     surname: "Curie",
+//     born: 1867,
+//     dead: 1934,
+//     id: 4,
+//   },
+//   {
+//     name: "Johannes",
+//     surname: "Kepler",
+//     born: 1571,
+//     dead: 1630,
+//     id: 5,
+//   },
+//   {
+//     name: "Nicolaus",
+//     surname: "Copernicus",
+//     born: 1473,
+//     dead: 1543,
+//     id: 6,
+//   },
+//   {
+//     name: "Max",
+//     surname: "Planck",
+//     born: 1858,
+//     dead: 1947,
+//     id: 7,
+//   },
+//   {
+//     name: "Katherine",
+//     surname: "Blodgett",
+//     born: 1898,
+//     dead: 1979,
+//     id: 8,
+//   },
+//   {
+//     name: "Ada",
+//     surname: "Lovelace",
+//     born: 1815,
+//     dead: 1852,
+//     id: 9,
+//   },
 
-  {
-    name: "Lise",
-    surname: "Meitner",
-    born: 1878,
-    dead: 1968,
-    id: 11,
-  },
-  {
-    name: "Sarah E.",
-    surname: "Goode",
-    born: 1855,
-    dead: 1905,
-    id: 10,
-  },
-  {
-    name: "Hanna",
-    surname: "Hammarström",
-    born: 1829,
-    dead: 1909,
-    id: 12,
-  },
-];
+//   {
+//     name: "Lise",
+//     surname: "Meitner",
+//     born: 1878,
+//     dead: 1968,
+//     id: 11,
+//   },
+//   {
+//     name: "Sarah E.",
+//     surname: "Goode",
+//     born: 1855,
+//     dead: 1905,
+//     id: 10,
+//   },
+//   {
+//     name: "Hanna",
+//     surname: "Hammarström",
+//     born: 1829,
+//     dead: 1909,
+//     id: 12,
+//   },
+// ];
 
 // ЗАДАЧА 3
 // МАЄМО МАСИВ, ПОТРІБНО:
@@ -236,55 +236,63 @@ const scientists = [
 // console.log(researchers)
 
 ДЗ:
-1 добавити хто саме з них попадає в даний проміжок активності
+// 1 добавити хто саме з них попадає в даний проміжок активності
 
-ЗАДАЧА 4
-Сортування примітивів.
-Виконати сортування масиву цін зі спадання та зростання.
-const prices = [2, 14, 1, 37, 26, 8];
+// ЗАДАЧА 4
+// Сортування примітивів.
+// Виконати сортування масиву цін зі спадання та зростання.
+// const prices = [2, 14, 1, 37, 26, 8];
 
-ЗАДАЧА 5
-Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
-const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
+// ЗАДАЧА 5
+// Виконати сортування масиву назв моніторів в алфавітному та зворотному алфавітному порядку.
+// const items = ["SAMSUNG", "LG", "ASUS", "DELL", "BENQ"];
 
-ЗАДАЧА 6
-Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
-Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
+// ЗАДАЧА 6
+// Зібрати в allTopics масив усіх предметів всіх курсів використовуючи flatMap.
+// Використовуючи Array.prototype.filter виконати фільтрацію, залишивши в uniqueTopics тільки унікальні елементи.
 
-const courses = [
-  {
-    name: "Basic HTML+CSS",
-    topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
+// const courses = [
+//   {
+//     name: "Basic HTML+CSS",
+//     topics: ["VSCode", "HTML", "CSS", "GitHub Desktop", "GitHub"],
+//   },
+//   {
+//     name: "Intermediate HTML+CSS",
+//     topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
+//   },
+//   {
+//     name: "Basic JavaScript",
+//     topics: [
+//       "VSCode",
+//       "Type system",
+//       "Loops",
+//       "Functions",
+//       "Conditions",
+//       "Classes",
+//       "DOM",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+//   {
+//     name: "Intermediate JavaScript",
+//     topics: [
+//       "VSCode",
+//       "NPM",
+//       "Bundlers",
+//       "Transpiling",
+//       "Promises",
+//       "AJAX",
+//       "Git",
+//       "GitHub",
+//     ],
+//   },
+// ];
+const object = {
+  message: 'Hello, World',
+  getMessage() {
+    const message = 'Hello, Earth';
+    return this.message;
   },
-  {
-    name: "Intermediate HTML+CSS",
-    topics: ["VSCode", "Terminal", "Git", "GitHub", "HTML", "CSS"],
-  },
-  {
-    name: "Basic JavaScript",
-    topics: [
-      "VSCode",
-      "Type system",
-      "Loops",
-      "Functions",
-      "Conditions",
-      "Classes",
-      "DOM",
-      "Git",
-      "GitHub",
-    ],
-  },
-  {
-    name: "Intermediate JavaScript",
-    topics: [
-      "VSCode",
-      "NPM",
-      "Bundlers",
-      "Transpiling",
-      "Promises",
-      "AJAX",
-      "Git",
-      "GitHub",
-    ],
-  },
-];
+};
+console.log(object.getMessage());
