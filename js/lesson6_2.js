4c531a4c8dcf1238d425398ae055d5e44d87ee02
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-29 12:49:15 +0300

    task 2
    solved

---
+++ js/lesson6_2.js
@@ -0,0 +1,87 @@
+// ЗАДАЧА 2
+// ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ
+//   https://www.thecocktaildb.com/api.php 
+// , ТА ЗА ДОПОМОГОЮ fetch, ВИВОДЬ РЕЗУЛЬТАТ НА ЕКРАН.
+// РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА, 
+// КОЛИ КЛІЄНТ НАБРАВ ДАНИЙ ТОВАР
+// (ВИКОРИСТАЙ РОЗМІТКУ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА)
+
+//   <div id='root'>
+//     <form id='form'>
+//         <input type="text" id="input">
+//       <button>Пошук</button>
+//     </form>
+//   </div>
+
+// idDrink
+// : 
+// "17253"
+// strAlcoholic
+// : 
+// "Alcoholic"
+// strCategory
+// : 
+// "Cocktail"
+// strCreativeCommonsConfirmed
+// : 
+// "No"
+// strDrink
+// : 
+// "Paloma"
+// strDrinkAlternate
+// : 
+// null
+// strDrinkThumb
+// : 
+// "https://www.thecocktaildb.com/images/media/drink/samm5j1513706393.jpg"
+// strGlass
+// : 
+// "Collins glass"
+// strIBA
+// : 
+// null
+// strImageAttribution
+// : 
+// null
+// strImageSource
+// : 
+// null
+// strIngredient1
+// : 
+// "Grape Soda"
+// strIngredient2
+// : 
+// "Tequila"
+const formEl= document.querySelector('#form');
+const inputEl= document.querySelector('#input');
+const drinksListEL=document.querySelector('.dinks_list');
+
+formEl.addEventListener('submit', onFormSubmit)
+function onFormSubmit(evt){
+    evt.preventDefault();
+    const imputValue= inputEl.value;
+    console.log('imputValue:',imputValue);
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${imputValue}`)
+    .then(resp=>resp.json())
+    .then(data=>renderMarkup(data.drinks))
+    // .then(data=>console.log(data.drinks))
+    // .then(list=>console.log(drinksListEL
+    //     .insertAdjacentHTML('beforeend', createMarkup({strDrink,strCategory,strAlcoholic,strDrinkThumb}))))
+    .catch(e=>console.log(e));
+    
+}
+function createMarkup({strDrink,strCategory,strAlcoholic,strDrinkThumb}){
+const markup=`
+<div class="coctailCard">
+    <img src="${strDrinkThumb}"
+     alt="${strDrink}"
+     class="drinkThumb">
+     <h2 class="drinkName">${strDrink}</h2>
+     <p class="Category">${strCategory}</p>
+     <p class="alcoholicStatus">${strAlcoholic}</p>
+</div>`
+drinksListEL.insertAdjacentHTML('beforeend', markup)
+}
+function renderMarkup(array){
+    array.forEach(element => createMarkup(element));
+}
