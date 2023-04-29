55e1779a7fff3cc28440233a1ed671d8550d75fb
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-04-29 19:13:30 +0300

    Update lesson6_2.js
    adds 2-d variant of solvation
    and searching by name

--- js/lesson6_2.js
+++ js/lesson6_2.js
@@ -1,8 +1,8 @@
 // // ЗАДАЧА 2
 // // ВИКОРИСТОВУЮЧИ API ДАНОГО ПОСИЛАННЯ
-// //   https://www.thecocktaildb.com/api.php 
+// //   https://www.thecocktaildb.com/api.php
 // // , ТА ЗА ДОПОМОГОЮ fetch, ВИВОДЬ РЕЗУЛЬТАТ НА ЕКРАН.
-// // РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА, 
+// // РЕЗУЛЬТАТ ПОШУКУ МОЖНА ОТРИМАТИ ІЗ ІМПУТА,
 // // КОЛИ КЛІЄНТ НАБРАВ ДАНИЙ ТОВАР
 // // (ВИКОРИСТАЙ РОЗМІТКУ ЗА ДОПОМОГОЮ ШАБЛОННОГО РЯДКА)
 
@@ -14,62 +14,62 @@
 // //   </div>
 
 // // idDrink
-// // : 
+// // :
 // // "17253"
 // // strAlcoholic
-// // : 
+// // :
 // // "Alcoholic"
 // // strCategory
-// // : 
+// // :
 // // "Cocktail"
 // // strCreativeCommonsConfirmed
-// // : 
+// // :
 // // "No"
 // // strDrink
-// // : 
+// // :
 // // "Paloma"
 // // strDrinkAlternate
-// // : 
+// // :
 // // null
 // // strDrinkThumb
-// // : 
+// // :
 // // "https://www.thecocktaildb.com/images/media/drink/samm5j1513706393.jpg"
 // // strGlass
-// // : 
+// // :
 // // "Collins glass"
 // // strIBA
-// // : 
+// // :
 // // null
 // // strImageAttribution
-// // : 
+// // :
 // // null
 // // strImageSource
-// // : 
+// // :
 // // null
 // // strIngredient1
-// // : 
+// // :
 // // "Grape Soda"
 // // strIngredient2
-// // : 
+// // :
 // // "Tequila"
-// const formEl= document.querySelector('#form');
-// const inputEl= document.querySelector('#input');
-// const drinksListEL=document.querySelector('.dinks_list');
+const formEl = document.querySelector("#form");
+const inputEl = document.querySelector("#input");
+const drinksListEL = document.querySelector(".dinks_list");
 
-// formEl.addEventListener('submit', onFormSubmit)
-// function onFormSubmit(evt){
-//     evt.preventDefault();
-//     const imputValue= inputEl.value;
-//     console.log('imputValue:',imputValue);
-//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${imputValue}`)
-//     .then(resp=>resp.json())
-//     .then(data=>renderMarkup(data.drinks))
-//     // .then(data=>console.log(data.drinks))
-//     // .then(list=>console.log(drinksListEL
-//     //     .insertAdjacentHTML('beforeend', createMarkup({strDrink,strCategory,strAlcoholic,strDrinkThumb}))))
-//     .catch(e=>console.log(e));
-    
-// }
+formEl.addEventListener("submit", onFormSubmit);
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const imputValue = inputEl.value.trim();
+  console.log("imputValue:", imputValue);
+  fetch(
+    // `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${imputValue}`
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${imputValue}`
+  )
+    .then((resp) => resp.json())
+    .then((data) => renderMarkup(data.drinks))    
+    .catch((e) => console.log(e));
+}
+
 // function createMarkup({strDrink,strCategory,strAlcoholic,strDrinkThumb}){
 // const markup=`
 // <div class="coctailCard">
@@ -82,9 +82,32 @@
 // </div>`
 // drinksListEL.insertAdjacentHTML('beforeend', markup)
 // }
+
 // function renderMarkup(array){
 //     array.forEach(element => createMarkup(element));
 // }
+////////////////////
+/////Variant 2//////
+////////////////////
+function createMarkup(array) {
+  return array
+    .map(({ strDrink, strCategory, strAlcoholic, strDrinkThumb }) => {
+      return `
+    <div class="coctailCard">
+        <img src="${strDrinkThumb}"
+          alt="${strDrink}"
+          class="drinkThumb">
+        <h2 class="drinkName">${strDrink}</h2>
+        <p class="Category">${strCategory}</p>
+        <p class="alcoholicStatus">${strAlcoholic}</p>
+    </div>`;
+    })
+    .join("");
+}
+
+function renderMarkup(array) {
+    drinksListEL.insertAdjacentHTML('beforeend', createMarkup(array));
+}
 
 // ЗАДАЧА 3
 // НАПИШІТЬ ПАГІНАЦІЮ, ДЛЯ ПЕРЕХОДУ ПО СТОРІНКАХ
